1e3638c330a00e66b90c09b5cc8635a37143ed17
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-09-07 13:07:00 -0500

    Database done

---
+++ game-kai-workspace/game-kai-workspace/Database.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using game_kai_workspace.Models;
+using SQLite;
+
+namespace game_kai_workspace
+{
+    public static class Database
+    {
+        static SQLiteAsyncConnection dbConnection;
+        
+        static async Task InitializeDb()
+        {
+            if (dbConnection != null)
+                return;
+            
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ItemsDatabase.db3");
+            
+            dbConnection = new SQLiteAsyncConnection(databasePath);
+
+            await dbConnection.CreateTableAsync<Item>();
+            
+            if (await dbConnection.Table<Item>().CountAsync() > 0)
+                return;
+            
+            await SeedDatabase();
+        }
+
+        private static async Task SeedDatabase()
+        {
+            var items = new List<Item>
+            {
+                new Item { Id = "1", Name = "Briefcase", ImageSource = "briefcase.png", Description="Your trusty old case. Battered and worn.", Status = Item.ItemStatus.Obtained },
+                new Item { Id = "2", Name = "9mm Handgun", ImageSource = "gun.png", Description="Standard issue for an investigator doing dangerous work.", Status = Item.ItemStatus.Obtained },
+                new Item { Id = "3", Name = "Notepad", ImageSource = "notepad.png", Description="Your notes are comprehensive.", Status = Item.ItemStatus.Obtained },
+                new Item { Id = "4", Name = "Pills", ImageSource = "pills.png", Description="Probably home-brewed. What was Kai working on?", Status = Item.ItemStatus.Unobtained },
+                new Item { Id = "5", Name = "SD-Card", ImageSource = "sd-card.png", Description="A secret memory card hidden in a vintage camera.", Status = Item.ItemStatus.Unobtained },
+                new Item { Id = "6", Name = "Photograph", ImageSource = "portrait.png", Description="Looks like a younger version of Kai...possibly her younger sister.", Status = Item.ItemStatus.Unobtained },
+            };
+            
+            await dbConnection.InsertAllAsync(items);
+        }
+        
+        // Task or Task<List<Item>> or Task<IEnumerable<Item>>? What's the difference?
+        public static async Task<IEnumerable<Item>> GetItems()
+        {
+            await InitializeDb();
+            
+            var items = await dbConnection.Table<Item>().ToListAsync();
+            
+            return items;
+        }
+        
+        public static async Task UpdateItem()
+        {
+            await InitializeDb();
+        }
+    }
+}
